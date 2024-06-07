5d71e446405ada776091e2b6e82f868caab2e791
Mirko Bozzetto <mirkouch@gmail.com>
2024-06-07 22:07:44 +0200

    chore: Update CSS and remove unused code

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="flex items-center py-4 w-full">
+      <h1 className="inline-flex items-center gap-2 font-bold text-lg">
+        <span>React Journey</span>
+      </h1>
+    </header>
+  );
+};
+export default Header;
