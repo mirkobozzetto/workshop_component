4e67ad58783051e8cfb41a47ee8bbfabdc0369e5
Mirko Bozzetto <mirkouch@gmail.com>
2024-06-07 22:56:07 +0200

    chore: Remove unused code and update Button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,23 +1,3 @@
 import { cva } from "class-variance-authority";
 
-export const buttonVariants = cva({
-  primary: {
-    backgroundColor: "blue",
-    color: "white",
-  },
-  secondary: {
-    backgroundColor: "white",
-    color: "black",
-  },
-  tertiary: {
-    backgroundColor: "transparent",
-    color: "blue",
-  },
-});
-
-const Button = () => {
-  return (
-//
-  );
-};
-export default Button;
+export const buttonVariants = cva("");
