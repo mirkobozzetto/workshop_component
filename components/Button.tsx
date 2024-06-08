d766c3be45ce9efc61ec3a1a38f1abd71e570014
Mirko Bozzetto <mirkouch@gmail.com>
2024-06-08 14:00:05 +0200

    chore: Update Button and Header components

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,12 +2,14 @@ import { cva } from "class-variance-authority";
 
 type ButtonProps = {
   children: React.ReactNode;
-  classname?: string;
+  className?: string;
 };
 
-export const buttonVariants = cva("");
+export const buttonVariants = cva(
+  "items-center inline-flex justify-center rounded-md px-3 py-1.5"
+);
 
-const Button = ({ children, classname, ...props }: ButtonProps) => {
-  return <button className={buttonVariants()}>{children}</button>;
+const Button = ({ children, className, ...props }: ButtonProps) => {
+  return <button className={buttonVariants({ className })}>{children}</button>;
 };
 export default Button;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,21 @@
+import { ShoppingBasket, User } from "lucide-react";
 import { ReactSvg } from "./ReactSvg";
-// import { ShoppingBasket, User } from "lucide-react";
 
 const Header = () => {
   return (
     <header className="flex items-center py-4 w-full">
       <h1 className="inline-flex items-center gap-2 font-bold text-lg">
         <ReactSvg size={32} />
-        <span>React Journey</span>
+        <span>ReactJourney</span>
       </h1>
+      <div className="flex items-center gap-2 ml-auto">
+        <button>
+          <ShoppingBasket size={24} />
+        </button>
+        <button>
+          <User size={24} />
+        </button>
+      </div>
     </header>
   );
 };
