217db63e51821e834fd61385d0429e0ea3cbc21a
Mirko Bozzetto <mirkouch@gmail.com>
2024-06-10 12:38:00 +0200

    chore: Update ReactCard component to include a link and fix styling

--- components/ReactCard.tsx
+++ components/ReactCard.tsx
@@ -1,4 +1,5 @@
 import { PlusCircle } from "lucide-react";
+import Link from "next/link";
 import Button from "./Button";
 
 import { ReactSvg } from "./ReactSvg";
@@ -12,7 +13,7 @@ type CardProps = {
 const ReactCard = ({ card }: { card: CardProps }) => {
   return (
     //
-    <div className="flex flex-col justify-center items-center gap-4 border-4 hover:border-gray-300 hover:bg-gray-100 shadow my-2 p-4 rounded-lg w-80 transition-colors">
+    <div className="flex flex-col justify-center items-center gap-4 border-4 hover:border-gray-300 hover:bg-gray-100 shadow my-2 p-4 rounded-lg w-auto transition-colors">
       <div className="flex items-center gap-2 w-full">
         <ReactSvg size={24} />
         <p className="font-bold text-base">React</p>
@@ -24,7 +25,11 @@ const ReactCard = ({ card }: { card: CardProps }) => {
         <p className="line-clamp-1 text-gray-400 text-start text-xs">
           {card.category}
         </p>
-        <Button variant="secondary">
+        <Link href={card.url}>{card.url}</Link>
+        <Button
+          className="hover:bg-gray-150 no-outline focus:outline-none focus:ring-0 focus:ring-offset-0 cursor-pointer"
+          variant="secondary"
+        >
           <PlusCircle size={16} />
         </Button>
       </div>
